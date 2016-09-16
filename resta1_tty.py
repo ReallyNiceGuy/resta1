77836176afb91f6835fcb0761e09d0d7820b1979
Marco Aurelio da Costa <costa@gamic.com>
2016-09-16 11:28:31 -0300

    Define main

--- resta1_tty.py
+++ resta1_tty.py
@@ -61,55 +61,57 @@ def reverse():
 def normal():
   ansi(ESC,"39;49m")
 
-undo=[]
-cls()
-l,c=(0,0)
-b = Board()
-selected=False
-home()
-print(b.saveGame())
-while b.hasValidMoves():
-  if selected:
-    reverse()
-    moveTo(origin[1],origin[0])
-    print(b.getCell(origin[0],origin[1]),end="")
-    normal()
-  moveTo(l,c)
-  ch = getKey() 
-  if ch == ord("q"):
-    moveTo(b.numLines,0)
-    ansi("Sair realmente [Y/n]?")
-    ch = getch()
-    ansi(ESC,"1K")
-    if ch != "n":
-      break
-  elif ch == K_UP or ch == ord("i"):
-    l=max(0,l-1)
-  elif ch == K_DOWN or ch == ord("k"):
-    l=min(b.numLines-1,l+1)
-  elif ch == K_LEFT or ch == ord("j"):
-    c=max(0,c-1)
-  elif ch == K_RIGHT or ch == ord("l"):
-    c=min(b.numCols-1,c+1)
-  elif ch == ord(" ") or ch == 13:
-    if not selected:
-      if len(b.getPossibleMoves(c,l))>0:
-        origin=(c,l)
-        selected = True
-    else:
-      selected = False
-      target=(c,l)
-      sg = b.saveGame()
-      if b.move(origin,target):
-        undo.append(sg) 
-  elif not selected and ch == ord("u") and len(undo)>0:
-    b.loadGame(undo.pop())
+def main():
+  undo=[]
+  cls()
+  l,c=(0,0)
+  b = Board()
+  selected=False
   home()
   print(b.saveGame())
+  while b.hasValidMoves():
+    if selected:
+      reverse()
+      moveTo(origin[1],origin[0])
+      print(b.getCell(origin[0],origin[1]),end="")
+      normal()
+    moveTo(l,c)
+    ch = getKey() 
+    if ch == ord("q"):
+      moveTo(b.numLines,0)
+      ansi("Sair realmente [Y/n]?")
+      ch = getch()
+      ansi(ESC,"1K")
+      if ch != "n":
+        break
+    elif ch == K_UP or ch == ord("i"):
+      l=max(0,l-1)
+    elif ch == K_DOWN or ch == ord("k"):
+      l=min(b.numLines-1,l+1)
+    elif ch == K_LEFT or ch == ord("j"):
+      c=max(0,c-1)
+    elif ch == K_RIGHT or ch == ord("l"):
+      c=min(b.numCols-1,c+1)
+    elif ch == ord(" ") or ch == 13:
+      if not selected:
+        if len(b.getPossibleMoves(c,l))>0:
+          origin=(c,l)
+          selected = True
+      else:
+        selected = False
+        target=(c,l)
+        sg = b.saveGame()
+        if b.move(origin,target):
+          undo.append(sg) 
+    elif not selected and ch == ord("u") and len(undo)>0:
+      b.loadGame(undo.pop())
+    home()
+    print(b.saveGame())
 
-moveTo(b.numLines,0)
-if b.resta1():
-  print("Parabéns!")
-elif not b.hasValidMoves():
-  print("Tente novamente")
+  moveTo(b.numLines,0)
+  if b.resta1():
+    print("Parabéns!")
+  elif not b.hasValidMoves():
+    print("Tente novamente")
 
+main()
